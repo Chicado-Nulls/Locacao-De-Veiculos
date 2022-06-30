9681e894fc7b63dde86bb6ebfdc4edd0451293a4
Marcos Lima <developermarcos@outlook.com>
2022-06-30 17:21:12 -0300

    Tabela Condutor criada

---
+++ Locadora.Infra.Sql/TbCondutor.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TbCondutor]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NCHAR(60) NOT NULL, 
+    [Cpf] NCHAR(14) NOT NULL, 
+    [Cnh] NCHAR(11) NOT NULL, 
+    [Endereco] NCHAR(150) NOT NULL, 
+    [Email] NCHAR(80) NOT NULL, 
+    [Telefone] NCHAR(15) NOT NULL, 
+    [Cliente_Id] INT NOT NULL, 
+    CONSTRAINT [FK_TbCondutor_TbCliente] FOREIGN KEY (Cliente_Id) REFERENCES [TbCliente]([Id])
+)
