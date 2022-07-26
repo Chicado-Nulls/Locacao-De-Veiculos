e07b731f26839cde2105e623b559541334e6f3e2
Marcos Adriano Ribeiro Lima <developermarcos@outlook.com>
2022-07-26 14:52:42 -0300

    Testes Repositorio Funcionario Orm implementado

---
+++ Locadora.Test.Infra.Orm/Compartilhado/RepositorioBaseOrmTest.cs
@@ -0,0 +1,48 @@
+﻿using Locadora.Infra.Orm.Compartilhado;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Test.Infra.Orm.Compartilhado
+{
+    public class RepositorioBaseOrmTest
+    {
+        protected string connectionString;
+        protected LocadoraVeiculoDbContext contextoDadosOrm;
+        public RepositorioBaseOrmTest()
+        {
+            var configuracao = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("ConfiguracaoAplicacao.json")
+                .Build();
+
+            
+            connectionString = configuracao.GetConnectionString("SqlServer");
+
+            contextoDadosOrm = new LocadoraVeiculoDbContext(connectionString);
+
+            LocadoraVeiculosMigrador.AtualizarBancoDados();
+
+            LimparTabela("TBFUNCIONARIO");
+        }
+        protected void LimparTabela(string tabela)
+        {
+            string sql = @"DELETE FROM "+tabela;
+
+            SqlConnection conexaoComBanco = new SqlConnection(connectionString);
+
+            SqlCommand comando = new SqlCommand(sql, conexaoComBanco);
+
+            conexaoComBanco.Open();
+
+            comando.ExecuteNonQuery();
+
+            conexaoComBanco.Close();
+        }
+    }
+}
