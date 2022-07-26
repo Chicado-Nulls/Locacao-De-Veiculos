30324e813ae0fdb3b3cd268f51162d18431c431b
João Moraes <Joo Moraes@DESKTOP-B41A5GE>
2022-07-26 14:09:38 -0300

    Feature tabelaGrupoVeiculoOrm

---
+++ Locadora.Infra.Orm/Migrations/20220726164245_AdicionandoGrupoVeiculo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Locadora.Infra.Orm.Migrations
+{
+    public partial class AdicionandoGrupoVeiculo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBGrupoVeiculo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBGrupoVeiculo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veiculo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CapacidadeTanque = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    KmPercorrido = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    TipoDeCombustivel = table.Column<int>(type: "int", nullable: false),
+                    GrupoDeVeiculoId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Foto = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Veiculo_TBGrupoVeiculo_GrupoDeVeiculoId",
+                        column: x => x.GrupoDeVeiculoId,
+                        principalTable: "TBGrupoVeiculo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculo_GrupoDeVeiculoId",
+                table: "Veiculo",
+                column: "GrupoDeVeiculoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculo");
+
+            migrationBuilder.DropTable(
+                name: "TBGrupoVeiculo");
+        }
+    }
+}
