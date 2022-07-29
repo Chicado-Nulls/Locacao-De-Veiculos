73ec45d14bb0eb65acef0195b4982fa642e81fb6
João Moraes <Joo Moraes@DESKTOP-B41A5GE>
2022-07-29 16:25:29 -0300

    Implementado Interface GeradorPdf

--- Locadora.Apresentacao.WinForm/Compartilhado/GeradorDePdf.cs
+++ Locadora.Apresentacao.WinForm/Compartilhado/GeradorDePdf.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iTextSharp.text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Locadora.Apresentacao.WinForm.Compartilhado
 {
-    public class GeradorDePdf
+    public interface GeradorDePdf
     {
+       void ConfigurarDocumento();
+       void GerarHeader(Document document);
+       void GerarCorpo(Document document);
+       void GerarRodaPe(Document document);
     }
 }
---
+++ Locadora.Apresentacao.WinForm/Compartilhado/PdfLocação.cs
@@ -0,0 +1,41 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Apresentacao.WinForm.Compartilhado
+{
+    public class PdfLocação : GeradorDePdf
+    {
+        public void ConfigurarDocumento()
+        {
+            var pdf = new Document(PageSize.A4);
+            var NomeArquivo = $"Locação{DateTime.Now}.pdf";
+            var arquivo = new FileStream(NomeArquivo, FileMode.Create);
+            var writer = PdfWriter.GetInstance(pdf, arquivo);
+
+            pdf.Open();
+
+             
+        }
+
+        public void GerarCorpo(Document document)
+        {
+          
+        }
+
+        public void GerarHeader(Document document)
+        {
+            
+        }
+
+        public void GerarRodaPe(Document document)
+        {
+            
+        }
+    }
+}
